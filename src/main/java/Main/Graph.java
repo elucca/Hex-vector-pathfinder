fd3899e46444bec42305aca7709ec08a2431e8c6
Juho Kesälä <elukka.elukka@gmail.com>
2019-09-19 17:00:09 +0300

    Implement edge weights in graph

--- src/main/java/Main/Graph.java
+++ src/main/java/Main/Graph.java
@@ -1,5 +1,6 @@
 package Main;
 
+import Data.AxialCoords;
 import Data.CubeCoords;
 import Data.Node;
 import Utility.CoordTransform;
@@ -28,6 +29,8 @@ public class Graph {
     private int timeMul;
     private int deltaVMul;
 
+    private CubeCoords[] neighborDirections;
+
     /**
      * Constructs a graph instance representing the state space of a spaceship on the hex grid. In
      * order for the graph to have a finite size, map dimensions and velocity vector magnitude need
@@ -47,44 +50,57 @@ public class Graph {
 
         this.timeMul = timeMul;
         this.deltaVMul = deltaVMul;
-    }
 
-    /**
-     * Finds the neighbors of the given node and returns a list of them. Todo: Assign edge weights
-     * per cost function multipliers.
-     *
-     * @param node The node to find the neighbors of.
-     * @return A list of nodes neighboring the given node.
-     */
-    public List<Node> findNeighbors(Node node) {
-        // Todo: Break this up and make it more readable.
-        CubeCoords[] directions = new CubeCoords[]{
+        this.neighborDirections = new CubeCoords[]{
             new CubeCoords(1, -1, 0),
             new CubeCoords(1, 0, -1),
             new CubeCoords(0, 1, -1),
             new CubeCoords(-1, 1, 0),
             new CubeCoords(-1, 0, 1),
             new CubeCoords(0, -1, 1)};
+    }
 
+    /**
+     * Finds the neighbors of the given node and returns a list of them. The neighbors include the
+     * node the ship would travel to if it did not accelerate at all, as well as each node the ship
+     * would travel to if it accelerated in any of the six possible directions. These neighbors
+     * consist of the hex coordinate and the current vector modified by the same direction vector.
+     * (those in the neighborDirections array)
+     *
+     * Todo: Allow larger vector changes up to some limits. Currently it's hardcoded to 0 (if vector
+     * isn't changed) or 1 (if vector is changed.)
+     * Todo: Think how to handle if the path
+     * goes out of the map. This is the case when there are no neighbors. Todo: Clean this up and
+     * maybe break it up to make it more readable.
+     *
+     * @param node The node to find the neighbors of.
+     * @return A list of nodes neighboring the given node.
+     */
+    public List<Node> findNeighbors(Node node) {
         List<Node> neighbors = new ArrayList<>();
 
         // The node the ship gets to with the current vector is always a neighbor,
         // if it exists.
-        CubeCoords destination = VectorMath.sum(CoordTransform.axialToCube(node.getCoords()),
-                node.getVector());
-        Node potentialNeighbor = new Node(CoordTransform.cubeToAxial(destination), node.getVector(), node);
+        AxialCoords destination = CoordTransform.cubeToAxial(
+                VectorMath.sum(CoordTransform.axialToCube(node.getCoords()), node.getVector()));
+
+        Node potentialNeighbor = new Node(destination, node.getVector(), node, neighborCost(0));
         if (nodeExists(potentialNeighbor)) {
             neighbors.add(potentialNeighbor);
         }
 
-        for (CubeCoords direction : directions) {
-            potentialNeighbor = new Node(CoordTransform.cubeToAxial(VectorMath.sum(destination, direction)),
-                    VectorMath.sum(node.getVector(), direction), node);
+        for (CubeCoords direction : neighborDirections) {
+            // All these coord transforms are a mess, refactor to overload the math methods to accept
+            // either.
+            destination = CoordTransform.cubeToAxial(VectorMath.sum(CoordTransform.axialToCube(destination), direction));
+
+            potentialNeighbor = new Node(destination, VectorMath.sum(node.getVector(), direction), node, neighborCost(1));
             if (nodeExists(potentialNeighbor)) {
                 neighbors.add(potentialNeighbor);
             }
         }
 
+        // Node(AxialCoords coords, CubeCoords vector, Node previous, int costSoFar)
         return neighbors;
     }
 
@@ -92,8 +108,7 @@ public class Graph {
      * Checks if a node generated while generating neighbors is actually valid, i.e. if it should
      * exist according to the graph's map size and maximum velocity parameters.
      *
-     * Currently assumes a rhombus-shaped map. Todo: Rectangular maps, and an option to generate
-     * either.
+     * Currently assumes a rhombus-shaped map. Todo: Change to assume a rectangular map instead.
      *
      * @param node The node to be validated.
      * @return Whether the node exists.
@@ -109,6 +124,19 @@ public class Graph {
         }
 
         return true;
+    }
+
+    /**
+     * Returns the cost that would be incurred on the path to a neighboring node, i.e. cost from the
+     * start to the node currently being processed plus the edge weight to the neighbor. The cost is
+     * based on the graph's cost multipliers for time and delta-v, and the amount of delta-v spent.
+     *
+     * @param dVSpent The amount of delta-v to be spent on this transition (magnitude of vector
+     * change).
+     * @return The incurred cost so far to the neighboring node.
+     */
+    private int neighborCost(int dVSpent) {
+        return timeMul + deltaVMul * dVSpent;
     }
 
     public int getTimeMul() {
