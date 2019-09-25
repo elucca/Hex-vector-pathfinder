b645a9115872a650e923e8c1a56782404dc66627
Juho Kesälä <elukka.elukka@gmail.com>
2019-09-25 20:13:09 +0300

    Refactor test package structure to fit with previous commit

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -14,12 +14,12 @@ public class Main {
         // Temporarily testing pathfinding by hardcoding it there
         Graph graph = new Graph(11, 11, 10, 1, 0);
         Pathfinder pathfinder = new Pathfinder();
-        
+
         Node start = new Node(new AxialCoords(2, 0), new CubeCoords(0, 0, 0), null, 0);
         Node goal = new Node(new AxialCoords(0, 4), new CubeCoords(0, 0, 0), null, 0);
-        
+
         Node found = pathfinder.Dijkstra(graph, start, goal);
-        
+
         System.out.println("Path, end to start: ");
         Node node = found;
         while (node != null) {
--- src/test/java/Pathfinder/GraphTest.java
+++ src/test/java/Pathfinder/GraphTest.java
@@ -1,4 +1,4 @@
-package Main;
+package Pathfinder;
 
 import Pathfinder.Graph;
 import Data.AxialCoords;
