1be888801e2de46128bf30176a03910c627fd846
Juho Kesälä <elukka.elukka@gmail.com>
2019-09-27 16:14:44 +0300

    Finish preliminary (untested) implementation of a min heap

--- src/main/java/Utility/MinHeap.java
+++ src/main/java/Utility/MinHeap.java
@@ -7,12 +7,13 @@ import java.util.Comparator;
  * An implementation of a minimum heap for O(log(n)) retrieval of the graph node with the minimum
  * value, as determined by the specified comparator.
  *
- * Todo: Resize the heap if maxSize would be exceeded
+ * Todo: Handle the case where the heap cannot fit in the array. This will currently crash.
  */
 public class MinHeap {
 
     private Node[] heap;
     private int heapSize;
+    private int maxSize;
 
     private Comparator comparator;
 
@@ -24,8 +25,53 @@ public class MinHeap {
      */
     public MinHeap(int maxSize, Comparator comparator) {
         this.heap = new Node[maxSize + 1];
+        this.maxSize = maxSize;
         this.heapSize = 0;
+
         this.comparator = comparator;
+    }
+
+    /**
+     * Return the node with the minimum value, without removing it from the heap.
+     *
+     * @return The node with the minimum value currently in the heap.
+     */
+    public Node min() {
+        return heap[0];
+    }
+
+    /**
+     * Return the node with the minimum value and remove it from the heap.
+     *
+     * @return The node with the minimum value currently in the heap.
+     */
+    public Node delMin() {
+        Node min = heap[0];
+        heapSize--;
+        heapify(0);
+
+        return min;
+    }
+
+    /**
+     * Insert the given node into the heap.
+     *
+     * @param node The node to be inserted.
+     */
+    public void insert(Node node) {
+        if (heapSize >= maxSize) {
+            return;
+        }
+
+        heapSize++;
+        heap[heapSize] = node;
+        int i = heapSize;
+
+        // current < parent(current)
+        while (comparator.compare(i, parent(i)) == -1) {
+            swap(i, parent(i));
+            i = parent(i);
+        }
     }
 
     /**
@@ -37,25 +83,24 @@ public class MinHeap {
     private void heapify(int i) {
         int leftI = left(i);
         int rightI = right(i);
-        
+
         if (rightI <= heapSize) {
             int smallestI;
-            
+
             // If left is smaller than right
             if (comparator.compare(heap[leftI], heap[rightI]) == -1) {
                 smallestI = leftI;
             } else {
                 smallestI = rightI;
             }
-            
+
             // If heap[i] is smaller than smallest
             if (comparator.compare(heap[i], heap[smallestI]) == -1) {
-                // swap i and smallest
+                swap(i, smallestI);
                 heapify(smallestI);
             } else if (leftI == heapSize && comparator.compare(heap[leftI], heap[i]) == -1) {
-                // swap
+                swap(i, leftI);
             }
-            
         }
     }
 
@@ -67,7 +112,7 @@ public class MinHeap {
      * doesn't exist.
      */
     private int parent(int i) {
-        int parentIndex = i / 2;
+        int parentIndex = (i - 1) / 2;
         if (parentIndex >= heap.length || parentIndex < 0) {
             return -1;
         }
@@ -84,7 +129,7 @@ public class MinHeap {
      * child doesn't exist.
      */
     private int left(int i) {
-        int leftIndex = 2 * i;
+        int leftIndex = 2 * i + 1;
         if (leftIndex >= heap.length || leftIndex < 0) {
             return -1;
         }
@@ -101,12 +146,24 @@ public class MinHeap {
      * child doesn't exist.
      */
     private int right(int i) {
-        int rightIndex = 2 * i + 1;
+        int rightIndex = 2 * i + 2;
         if (rightIndex >= heap.length || rightIndex < 0) {
             return -1;
         }
 
         return rightIndex;
+    }
+
+    /**
+     * Swap the positions of the nodes at the given indices in the array used to store the heap.
+     *
+     * @param i The index of the first node.
+     * @param i2 The index of the second node.
+     */
+    private void swap(int i, int i2) {
+        Node temp = heap[i];
+        heap[i] = heap[i2];
+        heap[i2] = temp;
     }
 
 }
